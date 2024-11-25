156951a375df9f9e35e73ddd07ee96131856fdb0
jaemin <jaemin4033@gmail.com>
2024-11-26 01:18:37 +0900

    build test

---
+++ out/_next/static/chunks/app/page-9560b2711ea46671.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{1877:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6364,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[364,265,581,358],()=>s(1877)),_N_E=e.O()}]);
