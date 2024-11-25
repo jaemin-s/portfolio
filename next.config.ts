de14312185a035cef200f93319a7361bdb5e301a
jaemin <128450974+jaemin-s@users.noreply.github.com>
2024-11-26 07:52:47 +0900

    next.config.ts 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const prefix = process.env.NODE_ENV === 'production' ? 'https://jaemin-s.github.io/' : ''
+
 const nextConfig: NextConfig = {
   output: 'export', //static export 추가
   assetPrefix: '.', //상대경로
