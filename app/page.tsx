e992c4dbc7004834a5f7c8892a81b7a1eed1ad1f
jaemin <jaemin4033@gmail.com>
2024-11-27 02:34:03 +0900

    update: parallax background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import ParallaxContainer from "./presentation/component/ParallaxContainer";
+
 export default function Home() {
   return (
-    <div className="bg-gray-600 w-screen h-screen flex justify-center items-center ">
-      <p className="text-white text-4xl bg-inherit">CI/CD 준비 끝</p>
+    <div className="bg-gray-600 w-screen h-screen overflow-y-scroll">
+      <ParallaxContainer imgSrc={"https://github.com/jaemin-s/jaemin-s/raw/refs/heads/main/image/bg-01.webp"}>
+        <article className="h-screen flex items-center justify-center text-2xl">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </article>
+      </ParallaxContainer>
+      <ParallaxContainer imgSrc={"https://github.com/jaemin-s/jaemin-s/raw/refs/heads/main/image/bg-02.webp"}>
+        <article className="h-screen flex items-center justify-center text-2xl">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </article>
+      </ParallaxContainer>
+      <ParallaxContainer imgSrc={"https://github.com/jaemin-s/jaemin-s/raw/refs/heads/main/image/bg-03.webp"}>
+        <article className="h-screen flex items-center justify-center text-2xl">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </article>
+      </ParallaxContainer>
+      <ParallaxContainer imgSrc={"https://github.com/jaemin-s/jaemin-s/raw/refs/heads/main/image/bg-04.webp"}>
+        <article className="h-screen flex items-center justify-center text-2xl">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </article>
+      </ParallaxContainer>
+      <ParallaxContainer imgSrc={"https://github.com/jaemin-s/jaemin-s/raw/refs/heads/main/image/bg-05.webp"}>
+        <article className="h-screen flex items-center justify-center text-2xl">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </article>
+      </ParallaxContainer>
     </div>
   );
 }
---
+++ app/presentation/component/ParallaxContainer.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+
+interface ParallaxProps {
+    children? : ReactNode,
+    imgSrc : string
+}
+
+const ParallaxContainer: React.FC<ParallaxProps> = ({ children, imgSrc })  => {
+    
+    return (
+        <section 
+        className={'h-screen bg-cover relative parallaxGradient '}
+        style={{ backgroundImage: `url('${imgSrc}')`, backgroundAttachment: 'fixed' }}>
+            {children}
+        </section>
+    )
+}
+
+export default ParallaxContainer
